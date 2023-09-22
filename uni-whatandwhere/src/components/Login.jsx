03d5e2038e89f30cb3d0a51eaf2ec1de17ae0adc
Yusif-Aslan <262947@student.pwr.edu.pl>
2023-09-22 03:51:35 +0200

    Login and SignUP

--- uni-whatandwhere/src/components/Login.jsx
+++ uni-whatandwhere/src/components/Login.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import img from '../imgs/login.jpeg';
 
-function Login() {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setError('Incorrect email or password. Please try again.');
+  };
+
   return (
     <div>
-      <h1>Login Page</h1>
+      {/* <div className="image-container">
+        <img className="signup-img" src={img} alt="Some dummmy img" />
+      </div> */}
+      <div className="form-container login-form-container">
+        {/* <h2>Login</h2> */}
+        {error && <p className="error-message">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="form-input"
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="form-input"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="form-button">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
