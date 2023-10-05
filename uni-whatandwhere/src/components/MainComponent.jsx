b6683971f18f2e5fa2f496ce067c7e5caf00ae05
Atahanka <atahanka@gmail.com>
2023-10-05 14:18:51 +0200

    Main template done

--- uni-whatandwhere/src/components/MainComponent.jsx
+++ uni-whatandwhere/src/components/MainComponent.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
 const MainComponent = () => {
-  return (
-    <div>
-      {/* Your Main Component content goes here */}
-    </div>
-  );
-};
-
-export default MainComponent;
+    return (
+      <div>
+        {/* Top Bar Section */}
+        <div className="top-bar">
+          {/* Your top bar content goes here */}
+        </div>
+  
+        {/* Map Section */}
+        <div className="map-section">
+          {/* Your map content goes here */}
+        </div>
+  
+        {/* Filters Section */}
+        <div className="filters-section">
+          {/* Your filters content goes here */}
+        </div>
+  
+        {/* List Section */}
+        <div className="list-section">
+          {/* Your list content goes here */}
+        </div>
+      </div>
+    );
+  };
+  
+  export default MainComponent;
+  
--- uni-whatandwhere/src/pages/MainPage.jsx
+++ uni-whatandwhere/src/pages/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MainComponent from '.../components/MainComponent.jsx';
+import '../components/design-files-css/MainPage.css';
 
 const MainPage = () => {
   return (
