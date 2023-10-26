a27872ccdff598367e8bbf45f57cf1ea96ebef42
MPokotilov <mpokotilov2@gmail.com>
2023-10-26 18:56:45 +0200

    Signup and login page fixed

--- uni-whatandwhere/src/App.jsx
+++ uni-whatandwhere/src/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import EntryPage from './pages/EntryPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import SignupPage from './pages/SignupPage.jsx';
 import MainPage from './pages/MainPage.jsx';
+import TermsAndConditionsPage from './pages/TermsAndConditionsPage.jsx';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<EntryPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/mainpage" element={<MainPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<EntryPage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/signup" element={<SignupPage/>}/>
+                    <Route path="/mainpage" element={<MainPage/>}/>
+                    <Route path="/termsAndConditions" element={<TermsAndConditionsPage/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- uni-whatandwhere/src/components/SignupComponent.jsx
+++ uni-whatandwhere/src/components/SignupComponent.jsx
@@ -1,4 +1,3 @@
-// ExtendedLoginPage.jsx
 import React from 'react';
 import DynamicForm from '../components/DynamicForm';
 
@@ -7,18 +6,22 @@ const signupFields = [
     {name: 'surname', type: 'text', label: 'Surname', placeholder: 'Surname'},
     {name: 'username', type: 'text', label: 'Username', placeholder: 'Username'},
     {name: 'mail', type: 'email', label: 'Mail', placeholder: 'Email'},
+    {name: 'password', type: 'password', label: 'Password', placeholder: 'Password'},
     {name: 'phone', type: 'tel', label: 'Phone number', placeholder: 'Phone'},
     {
         name: 'dob',
-        type: 'date',
-        label: 'Date of Birth'
+        type: 'text',
+        label: 'Date of Birth',
+        placeholder: 'Birth Date',
+        onFocus: 'date',
+        onBlur: 'text'
     },
 ];
 
 function SignupComponent() {
     return (
         <div className="form-container signup-form-container">
-            <DynamicForm formData={signupFields} name={'Sign up'}/>
+            <DynamicForm formData={signupFields} name={'Sign up'} showTerms={true}/>
         </div>
     );
 }
