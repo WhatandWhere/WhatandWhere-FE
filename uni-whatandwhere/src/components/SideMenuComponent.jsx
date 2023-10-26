ce5659395957b6efc2b7795c727674f53e3a0772
Atahanka <atahanka@gmail.com>
2023-10-26 19:51:49 +0200

    side menu done

--- uni-whatandwhere/src/components/SideMenuComponent.jsx
+++ uni-whatandwhere/src/components/SideMenuComponent.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import '../components/design-files-css/SideMenu.css';
+import { Link } from 'react-router-dom';
 
 const SideMenuComponent = () => {
 
   return (
     <div className="side-menu">
       <ul>
-        <li>Page 1</li>
-        <li>Page 2</li>
-        <li>Page 3</li>
-        {/* Add more menu items as needed */}
+        <li><Link to="/login" className="nav-button">Login</Link></li>
+        <li><Link to="/signup" className="nav-button">Signup</Link></li>
+        <li><Link to="/" className="nav-button" onClick={() => { alert('Logged out'); }}>Home</Link></li>
       </ul>
     </div>
   );
