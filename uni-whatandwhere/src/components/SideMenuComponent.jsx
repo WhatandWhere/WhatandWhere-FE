79a9910e617cf73418cadf1a6aec4fe563471886
Atahanka <atahanka@gmail.com>
2023-10-26 16:38:20 +0200

    side menu 1

--- uni-whatandwhere/src/components/SideMenuComponent.jsx
+++ uni-whatandwhere/src/components/SideMenuComponent.jsx
@@ -5,7 +5,12 @@ const SideMenuComponent = () => {
 
   return (
     <div className="side-menu">
-      <h1>Side Menu</h1>
+      <ul>
+        <li>Page 1</li>
+        <li>Page 2</li>
+        <li>Page 3</li>
+        {/* Add more menu items as needed */}
+      </ul>
     </div>
   );
 };
