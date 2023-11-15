465f01871ec90112648489201d0ffbb80f1f3f11
DmitryRoox <dmitry.kuzma.05@gmail.com>
2023-11-15 18:26:13 +0100

    Whitespace on AboutUs section was added

--- src/components/entry-page/Button.jsx
+++ src/components/entry-page/Button.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../components/design-files-css/entry-page/Button.css";
 
-export const Button = ({ children, buttonStyle }) => {
+export const Button = ({ children, buttonStyle, buttonLink }) => {
   return (
-    <Link to="/">
+    <Link to={buttonLink ? buttonLink : ""}>
       <button className={buttonStyle}>{children}</button>
     </Link>
   );
