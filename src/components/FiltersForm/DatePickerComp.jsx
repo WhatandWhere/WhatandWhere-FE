d9f6181495a99cf2d436a343f5f43b326bc1ccea
DmitryRoox <dmitry.kuzma.05@gmail.com>
2023-11-17 23:17:56 +0100

    ++

--- src/components/FiltersForm/DatePickerComp.jsx
+++ src/components/FiltersForm/DatePickerComp.jsx
@@ -1,12 +1,10 @@
-import { React, useState } from "react";
+import React from "react";
 import { DatePicker } from "antd";
 import "../design-files-css/FiltersForm/DatePickerComp.css";
 
 const { RangePicker } = DatePicker;
 
 export default function DatePickerComp() {
-	const [dates, setDates] = useState([]);
-
 	return (
 		<div className="date-container">
 			<h3>Date of event</h3>
