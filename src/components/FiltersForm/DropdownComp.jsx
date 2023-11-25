9d198c1747708bc43584355dfc42ef4906e533b8
DmitryRoox <dmitry.kuzma.05@gmail.com>
2023-11-26 00:48:20 +0100

    Fetching categories from database is done

--- src/components/FiltersForm/DropdownComp.jsx
+++ src/components/FiltersForm/DropdownComp.jsx
@@ -1,6 +1,7 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import Select from "react-select";
-
+import axios from "axios";
+import { backendLink } from "../../action/authActions";
 import "../design-files-css/FiltersForm/DropdownComp.css";
 
 const options = [
@@ -14,6 +15,18 @@ const options = [
 
 export default function DropdownComp({ handelChange }) {
 	const [selectedOptions, setSelectedOptions] = useState([]);
+	const [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		axios.get(`${backendLink}/api/category`, {}).then((categories) => {
+			console.log(categories.data);
+			const categoryList = categories.data.map((category) => ({
+				value: category.categoryName,
+				label: category.categoryName,
+			}));
+			setCategories(categoryList);
+		});
+	}, []);
 
 	const handleChange = (selectedOption) => {
 		setSelectedOptions(selectedOption);
@@ -27,7 +40,7 @@ export default function DropdownComp({ handelChange }) {
 		<div className="category-container">
 			<h3>Category</h3>
 			<Select
-				options={options}
+				options={categories}
 				value={selectedOptions}
 				onChange={handleChange}
 				isMulti={multi}
